5e00dc24f00aa14a8340f68a299b6dc3a2f9833d
sanArch <88120180+ihsanpraditya@users.noreply.github.com>
2025-08-17 12:05:03 +0700

    change to lsp-config

--- init.lua
+++ init.lua
@@ -6,6 +6,6 @@ vim.g.maplocalleader = " "
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
-require("config.lsp")
+-- require("config.lsp")
 require("config.lazy")
 -- vim.loader.enable({ true }) -- might improve startup time but experimental
